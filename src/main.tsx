d0e8cbac3ff4d2ef4fc2cc8eec6969cb8b7fe085
Danilo Nobre Nunes <danilonobren@gmail.com>
2025-12-30 19:46:03 -0300

    fixing deploy stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ContrastChecker from '../wcag-contrast-checker';
+import ContrastChecker from './contrast-checker';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<React.StrictMode><ContrastChecker /></React.StrictMode>);
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
+  root: 'src',
   base: '/color-tester-that-doesnt-sucks/',
   plugins: [
     react(),
     tailwind(),
   ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 })
